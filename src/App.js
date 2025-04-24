c4566c1b8e56e825bf92ff331045e43e26f6c9dd
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-04-24 17:37:39 -0600

    applying transparency to the form, item components and linear gradient to main background.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import './App.scss';
-import Item from './Components/Item/Item.js';
-import Menu from './Components/Menu/Menu.js';
+import "./App.scss";
+import Item from "./Components/Item/Item.js";
+import Menu from "./Components/Menu/Menu.js";
 import TodoEntry from "./Components/Form/form.js";
 import Container from "react-bootstrap/Container";
-import {Col, Row} from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
-    return (
-        <div className="App">
-            <Menu></Menu>
-            <h1>ToDo List</h1>
-            <Container>
-                <Row>
-                    <Col xs={12} md={6} className="todo-entry"><TodoEntry></TodoEntry></Col>
-                    <Col xs={12} md={6} className="item-col">
-                        <Item></Item>
-                        <Item></Item>
-                        <Item></Item>
-                        <Item></Item>
-                        <Item></Item>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Menu></Menu>
+      <h1 className="main-title">ToDo List</h1>
+      <Container>
+        <Row>
+          <Col xs={12} md={6} className="todo-entry">
+            <TodoEntry></TodoEntry>
+          </Col>
+          <Col xs={12} md={6} className="item-col">
+            <Item></Item>
+            <Item></Item>
+            <Item></Item>
+            <Item></Item>
+            <Item></Item>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Form/form.js
+++ src/Components/Form/form.js
@@ -1,28 +1,32 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import "./form.scss"
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./form.scss";
 
 function TodoEntry() {
-    return (
-        <Form>
-            <Form.Group className="form-group mb-3" controlId="formBasicText">
-                <Form.Label>Task</Form.Label>
-                <Form.Control type="text" placeholder="Enter your task"/>
-            </Form.Group>
+  return (
+    <Form>
+      <Form.Group className="form-group mb-3" controlId="formBasicText">
+        <Form.Label>Task</Form.Label>
+        <Form.Control type="text" placeholder="Enter your task" />
+      </Form.Group>
 
-            <Form.Group className="form-group mb-3" controlId="formBasicText">
-                <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Enter your task description"/>
-            </Form.Group>
-            <Form.Group className="form-group date mb-3" controlId="formBasicDate">
-                <Form.Label>Due date</Form.Label>
-                <Form.Control type="date" placeholder="hola" min="2025-01-01"  max="2030-12-31"/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Add Task
-            </Button>
-        </Form>
-    );
+      <Form.Group className="form-group mb-3" controlId="formBasicText">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Enter your task description"
+        />
+      </Form.Group>
+      <Form.Group className="form-group date mb-3" controlId="formBasicDate">
+        <Form.Label>Due date</Form.Label>
+        <Form.Control type="date" min="2025-01-01" max="2030-12-31" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Add Task
+      </Button>
+    </Form>
+  );
 }
 
 export default TodoEntry;
